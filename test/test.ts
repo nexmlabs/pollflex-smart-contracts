de68e8011e2df65bd9ac20e337e99966ef688f94
Duy Trần <duytran9262002@gmail.com>
2023-06-07 00:12:35 +0700

    done smc

--- test/test.ts
+++ test/test.ts
@@ -5,7 +5,6 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("MarketPrediction", function () {
-
   async function deployTokenFixture() {
     const MarketPrediction = await ethers.getContractFactory("MarketPrediction");
 
@@ -15,7 +14,7 @@ describe("MarketPrediction", function () {
     return { hardhatMarketPrediction, owner, addr1, addr2 };
   }
 
-  it("should return the correct ipfsHash", async () => {
+  it("should return the correct ipfsHash when create vote", async () => {
     const { hardhatMarketPrediction, owner, addr1, addr2 } = await loadFixture(
       deployTokenFixture
     );
@@ -24,9 +23,41 @@ describe("MarketPrediction", function () {
     const bytes = ethers.utils.toUtf8Bytes(ipfsHash);
     await hardhatMarketPrediction.createVote(bytes);
 
-    const ipfsHashReceive = await hardhatMarketPrediction.getIpfsHash(1);
+    const bytes1 = ethers.utils.toUtf8Bytes(ipfsHash);
+    await hardhatMarketPrediction.createVote(bytes1);
+
+    const ipfsHashReceive = await hardhatMarketPrediction.getCreateVoted(2);
     const bytesRevert = ethers.utils.toUtf8String(ipfsHashReceive);
 
+    expect(ipfsHash).to.equal(bytesRevert);
+  });
+
+  it("should return the correct ipfsHash when submit vote", async () => {
+    const { hardhatMarketPrediction, owner, addr1, addr2 } = await loadFixture(
+      deployTokenFixture
+    );
+    /**Create */
+    const ipfsHashCreate = "Qmc45UMvYXRLhGiNchg2DH68eYg4aG2h9FSVJwb1yGhgbH";
+    const bytesCreate = ethers.utils.toUtf8Bytes(ipfsHashCreate);
+    await hardhatMarketPrediction.createVote(bytesCreate);
+
+
+    /**submit */
+    await hardhatMarketPrediction.addAdmin(addr1.address);
+    const blockNumber = await ethers.provider.getBlockNumber();
+    const nonce = await ethers.provider.getTransactionCount(owner.address, blockNumber);
+
+    const ipfsHash = "Qmc45UMvYXRLhGiNchg2DH68eYg4aG2h9FSVJwb1yGhgbH";
+    const bytes = ethers.utils.toUtf8Bytes(ipfsHash);
+    const encodedWithNonce = ethers.utils.solidityPack(["bytes", "uint256"], [bytes, nonce]);
+    const digest = ethers.utils.keccak256(encodedWithNonce);
+
+    const sign = await owner.signMessage(ethers.utils.arrayify(digest));
+    const sign1 = await addr1.signMessage(ethers.utils.arrayify(digest));
+
+    await hardhatMarketPrediction.submitVote(1, bytes, nonce, [sign, sign1]);
+    const ipfsHashReceive = await hardhatMarketPrediction.getsubmitVoted(1, owner.address);
+    const bytesRevert = ethers.utils.toUtf8String(ipfsHashReceive);
     expect(ipfsHash).to.equal(bytesRevert);
   });
 });
